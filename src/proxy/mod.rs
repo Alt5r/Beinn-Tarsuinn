0f52d9d44c4131811209d8e0ade4163b4e83cece
forge-633 <67239820+forge-633@users.noreply.github.com>
2024-12-16 17:58:31 +0000

    connection close seems to allow for more website compatability

--- src/proxy/mod.rs
+++ src/proxy/mod.rs
@@ -4,10 +4,19 @@ use std::net::{TcpListener, TcpStream};
 /// this will perform the proxying feature of the sofrwatere, acutally getting
 /// a respiksne for the the forwared request ? database implenetation
 pub fn forward(original_request: &str) -> std::io::Result<String> {
+    println!("forward has been called");
 
-    let trgt= "google.com:80";
+    let trgt= "en.wikipedia.org:80/wiki/United_Kingdom";
     let mut trgt_strm = TcpStream::connect(trgt)?;
-    trgt_strm.write_all(original_request.as_bytes());
+
+    let or = original_request;
+    let or = or.replace("localhost:8080", "en.wikipedia.org:80/wiki/United_Kingdom");
+    let or = or.replace("127.0.0.1:8080", "en.wikipedia.org:80/wiki/United_Kingdom");
+    let or = or.replace("Connection: keep-alive", "Connection: close");
+
+    println!("{}", or);
+
+    trgt_strm.write_all(or.as_str().as_bytes());
     trgt_strm.flush();
 
     let mut response_buffer = [0; 4096];
