4fade15340aabe0ead269762a74753291f6fac4e
Alt5r <rrow8nn@gmail.com>
2024-12-17 19:50:20 +0000

    changes for edges

--- src/rules/honeypot/mod.rs
+++ src/rules/honeypot/mod.rs
@@ -11,6 +11,11 @@ struct ThreatActor {
     ipv4:String,
 }
 
+#[derive(serde::Serialize, serde::Deserialize)]
+struct MaliciousSignature {
+    request:String
+}
+
 use surrealdb::engine::any;
 use surrealdb::Surreal;
 use surrealdb::opt::auth::Namespace;
@@ -21,7 +26,7 @@ pub async fn directoryChecker(
     request: HashMap<String, String>,
     client_addr: &SocketAddr
 ) -> Result<(), Box<dyn std::error::Error>> {
-    let key_words = ["username", "password", "cgi", "root", "admin", "cd"];
+    let key_words = ["username", "password", "cgi", "root", "admin", "cd", "wp"];
 
     // Connect to SurrealDB asynchronously
     let db = any::connect("wss://testing-honeypo-069tap2kaptiv68a587151t91o.aws-euw1.surreal.cloud/rpc").await?;
@@ -47,27 +52,42 @@ pub async fn directoryChecker(
             if req_string.contains(keyword) {
                 println!("Malicious request detected from {:?}!", client_addr);
                             // Create the ThreatActor instance
-                /* 
-                let threat_actor: ThreatActor = ThreatActor {
-                    ipv4: client_addr.to_string(),
-                };
-                */
-                let resc: &str = "threat-actors";
-                // Log or handle the malicious request here
-                //db.update::<ThreatActor>("threat-actors").merge(threat_actor).await?;
-
+                let ca = client_addr.to_string();
+                let ip_port = ca.split_once(":");
+                let ip = ip_port.unwrap().0.to_string();
                 //let ta: Option<ThreatActor> = db.create("threat-actor").await?;
 
                 let created: Option<ThreatActor> = db
-                .create("threat-actor")
+                .create("threat-actors")
                 .content(ThreatActor {
-                    ipv4:client_addr.to_string()
+                    ipv4:ip
                 })
                 .await?;
 
+                let mal_req: Option<MaliciousSignature> = db
+                .create("malicious-requests")
+                .content(MaliciousSignature {
+                    request:req_string
+                })
+                .await?;
+                let edge_query = format!(r#"
+                LET ta = (SELECT id FROM threat-actors WHERE ipv4 = "{}") 
+                LET ms = (SELECT id FROM malicious-requests WHERE request = "{val}")
+                CREATE (ta->ms) 
+                "#, ip, val=req_string);
+        
+            // Execute the edge creation query (replace "some-ip" and "some-request" with actual values)
+                db.query(edge_query).await?;
+                
+            
+                    println!("Created edge between ThreatActor and MaliciousSignature");
+                } else {
+                    println!("unable to create edge");
+                }
+                return Ok(());
             }
         }
     }
 
     Ok(())
-}
+    }
